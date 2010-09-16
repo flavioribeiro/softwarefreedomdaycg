d245f1260a28d92f27644f38fa3c56e0148bd6db
Flávio Ribeiro <email@flavioribeiro.com>
2010-09-16 01:06:04 -0300

    Adjust the title

--- presentation.py
+++ presentation.py
@@ -31,20 +31,29 @@ class Intra(Layer):
     def __init__(self):
         super(Intra, self).__init__()
 
-        label = Label('Desenvolvimento de Jogos com Python e Cocos2d',
+        label = Label('''Desenvolvimento de Jogos''',
                 font_name = 'happy sans',
                 font_size=46,
                 anchor_x='center',anchor_y='center',
-                color=(0, 0, 0, 200)
+                color=(0, 0, 0, 255)
                 )
-        label.position = 300, 300
+        label.position = 1024/2, (728+70)/2
+        self.add(label)
+
+        label = Label('''com Python e Cocos2d''',
+                font_name = 'happy sans',
+                font_size=46,
+                anchor_x='center',anchor_y='center',
+                color=(0, 0, 0, 255)
+                )
+        label.position = 1024/2, (728-30)/2
         self.add(label)
 
 if __name__ == "__main__":
     font_path = os.path.join( os.path.dirname(__file__), 'media/fonts')
     font.add_directory(font_path)
 
-    director.init(resizable=True, width=1024, height=748)
+    director.init(resizable=True, width=1024, height=728, fullscreen=True)
     _scene = Scene(Presentation())
     _scene.add(Intra(), z=1)
     director.run(_scene)
